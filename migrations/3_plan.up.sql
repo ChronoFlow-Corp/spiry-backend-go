28c7a9ee597f4d0e601f019e04b1b679d094ef76
undefeel <amir_inalov@inbox.ru>
2025-10-05 13:39:16 +0300

    fix: convert price

--- migrations/3_plan.up.sql
+++ migrations/3_plan.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS plans (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     prompt_limit INT,
-    price DECIMAL(10, 2),
+    price DECIMAL(10, 2) default 0,
     features TEXT[],
     end_date TIMESTAMP,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE UNIQUE,
